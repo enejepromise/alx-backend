9083db8b3085e9c3c0fd7291cd6cfca61d66aeec
enejepromise <enejepromise@gmail.com>
2024-10-28 18:29:20 +0000

    pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' A function that takes two integer '''
+from typing import Tuple
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    ''' simple helper function '''
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
