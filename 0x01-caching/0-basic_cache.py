45ba9bf1df0ee0f53764855c2b0be86cea85ec86
enejepromise <enejepromise@gmail.com>
2024-10-30 23:49:49 +0000

    update

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 """Basic Cache module
 """
@@ -15,7 +16,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key is None or item is None:
+        if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
